6040360162f2f115dbc7f36a4f24ee6f31332cfa
rome6315 <romo6315@gmail.com>
2023-01-11 23:00:48 -0500

    added tokyonight as default colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,8 @@
 function Colors(color)
-	color = color or "rose-pine" -- make sure we always have nord to fall back on cuz screw built in colors
+	color = color or "tokyonight" -- make sure we always have tokyonight enabled by default 
 	vim.cmd.colorscheme(color)
 
 	-- uncomment for transparecny: 
-	
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 
