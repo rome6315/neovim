190f82d03074cb952829b34b69122733f9c26a6f
Roman Morasco <romanmorasco@pop-os.localdomain>
2023-01-12 23:09:39 -0500

    make nord default colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function Colors(color)
-	color = color or "rose-pine" -- make sure we always have nord to fall back on cuz screw built in colors
+	color = color or "nord" -- make sure we always have nord to fall back on cuz screw built in colors
 	vim.cmd.colorscheme(color)
 
 	-- uncomment for transparecny: 
